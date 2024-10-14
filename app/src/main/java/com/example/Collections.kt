8db7dd9a988abb927e4c36ddc1c420a0b053d8d1
ZHARFAN <zharfanwidyadana@gmail.com>
2024-10-14 08:07:50 +0700

    Commit kedua

---
+++ app/src/main/java/com/example/Collections.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication
+
+//List adalah Kumpulan data yang disusun secara berurutan dari
+//List bersifat ordered, artinya data yang dimasukkan akan memiliki posisi sesuai urutan
+//List bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat dapat diubah dan tidak dapat diubah
+//List Read-Only menggunakan listOf
+//List Mutable menggunakan mutableListOf
+
+fun ContohList() {
+    println("===List ===")
+    //List Read-Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    //List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    //Menambah data ke dalam List Mutable
+    shape.add("Circle")
+    println(shape)
+
+    //Menghapus data dari List Mutable
+    shape.remove("Triangle")
+    println(shape)
+
+    //Mengubah data di dalam List Mutable
+    shape[0] = "Oval"
+    println(shape)
+
+    //List Read-Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
+}
+
+//Set adalah Kumpulan data yang tidak memiliki posisi atau urutan tertentu dan hanya menyimpan data unik(tidak ada duplikasi)
+//Set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+//Set bersifat mutable da read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+//Set Read-Only menggunakan setOf
+//Set Mutable menggunakan mutableSetOf
+
